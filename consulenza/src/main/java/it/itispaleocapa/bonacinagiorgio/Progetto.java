5921c35ab213382dd2ab8a209d6c1602f289b1ea
Giorgiobon <101709335+Giorgiobon@users.noreply.github.com>
2023-05-21 13:15:23 +0000

    finalCommit

--- consulenza/src/main/java/it/itispaleocapa/bonacinagiorgio/Progetto.java
+++ consulenza/src/main/java/it/itispaleocapa/bonacinagiorgio/Progetto.java
@@ -15,24 +15,18 @@ public class Progetto
         this.oreProgetto = oreProgetto;
     }
 
-    public void aggiungiFunzionario(String cognome, String nome, boolean tipo, int valore1, int valore2)
+    public void aggiungiFunzionario(Funzionario x)
     {
-        Funzionario x = new Funzionario(cognome, nome, tipo);
-        x.setTariffaOraria(valore1, valore2);
         membriProgetto.add(x);
     }
 
-    public void aggiungiTecnico(String cognome, String nome, boolean tipo, int valore1, int valore2)
+    public void aggiungiTecnico(Tecnico x)
     {
-        Tecnico x = new Tecnico(cognome, nome, tipo);
-        x.setTariffaOraria(valore1, valore2);
         membriProgetto.add(x);
     }
 
-    public void aggiungiDirigente(String nome, String cognome, int valore1)
+    public void aggiungiDirigente(Dirigente x)
     {
-        Dirigente x = new Dirigente(cognome, nome);
-        x.setTariffaOraria(valore1);
         membriProgetto.add(x);
     }
 
--- consulenza/src/test/java/it/itispaleocapa/bonacinagiorgio/AppTest.java
+++ consulenza/src/test/java/it/itispaleocapa/bonacinagiorgio/AppTest.java
@@ -12,7 +12,35 @@ class AppTest {
      * Rigorous Test.
      */
     @Test
-    void testApp() {
+    void aggiungiFunzionario() {
+        Progetto x = new Progetto(200);
+        Funzionario y = new Funzionario("Bonacina", "Giorio", true);
+        y.setTariffaOraria(10, 20);
+        x.aggiungiFunzionario(y);
+        assertEquals(x.membriProgetto.contains(y), true);
+    }
+
+    @Test
+    void aggiungiTecnico() {
+        Progetto x = new Progetto(200);
+        Tecnico y = new Tecnico("Bonacina", "Giorio", true);
+        y.setTariffaOraria(10, 20);
+        x.aggiungiTecnico(y);
+        assertEquals(x.membriProgetto.contains(y), true);
+    }
+
+    @Test
+    void aggiungiDirigente() {
+        Progetto x = new Progetto(200);
+        Dirigente y = new Dirigente("Bonacina", "Giorio");
+        y.setTariffaOraria(100);
+        x.aggiungiDirigente(y);
+        assertEquals(x.membriProgetto.contains(y), true);
+    }
+
+    @Test
+    void calcolaCostoProgetto() {
+        Progetto x = new Progetto(200);
         assertEquals(1, 1);
     }
 }
